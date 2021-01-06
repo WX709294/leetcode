5d60c6add16bad97ae6d9aa9324e8445a1a7fb9a
wuxue <wuxue@windimg.com>
2021-01-06 20:43:34 +0800

    题目

--- 剑指offer/二维数组中的查找.js
+++ 剑指offer/二维数组中的查找.js
@@ -3,6 +3,18 @@
 // 当前比目标大, 如果目标存在, 只能在左下边, 此时范围缩小一列
 // 当前比目标小, 目标存在的话, 只能在下边, 当前行不存在目标值, 此时范围缩小一行
 
+
+let matrix = [
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+let target = 5
+// 给定 target = 5，返回 true
+// 给定 target = 20，返回 false
+
 var findNumberIn2DArray = function(matrix, target) {
   let m = matrix.length
   if(!m) return false
